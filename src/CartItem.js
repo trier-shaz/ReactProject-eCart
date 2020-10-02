885c3ddb88f1bccf1be7b3d98bffca23b5fb479d
trier-shaz <shazakhtar14@gmail.com>
2020-10-02 20:12:20 +0530

    Add State to my cart

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,18 +1,43 @@
 import React from 'react';
 
 class CartItem extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            price: 999,
+            title: 'Mobile Phone',
+            qty: 1,
+            img: ''
+        }
+    }
     render(){
+        const { price, title, qty } = this.state;
         return (
             <div className="cart-item">
               <div className="left-block">
                 <img style={styles.image}/>
               </div>
               <div className="right-block">
-                <div style={ { fontSize: 25 } }>Phone</div>
-                <div style={ { color: '#777' } }>Rs 999</div>
-                <div style={ { color: '#777' } }>Qty: 1</div>
+                <div style={ { fontSize: 25 } }>{title}</div>
+                <div style={ { color: '#777' } }>Rs {price}</div>
+                <div style={ { color: '#777' } }>Qty: {qty}</div>
                 <div className="cart-item-actions">
                   {/* Buttons */}    
+                  <img 
+                    alt="increase" 
+                    className="actions-icons" 
+                    src="https://www.flaticon.com/svg/static/icons/svg/1828/1828926.svg" 
+                  />
+                  <img 
+                    alt="decrease" 
+                    className="actions-icons" 
+                    src="https://www.flaticon.com/svg/static/icons/svg/992/992683.svg" 
+                  />
+                  <img 
+                    alt="delete" 
+                    className="actions-icons" 
+                    src="https://www.flaticon.com/svg/static/icons/svg/1214/1214428.svg" 
+                  />
                 </div>
               </div>
             </div>
@@ -22,9 +47,9 @@ class CartItem extends React.Component{
 
 const styles = {
     image: {
-        height: 110,
-        width: 110,
-        borderRadius: 4,
+        height: 120,
+        width: 120,
+        borderRadius: 6,
         background: '#ccc'
     }
 }
